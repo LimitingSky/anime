33ba1f50eea6486f2d0a84447b14293b77715a9c
Luis Garcia <un.garcia9719@gmail.com>
2021-06-11 12:25:10 -0600

    add navigate generin inteface

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,6 +6,11 @@ import HomeView from '@screens/app/home';
 import { HOME_VIEW, TIME_LINE_VIEW } from './types';
 import TimeLineView from 'screens/app/timeLine';
 
+export interface INavigateMethod {
+	screen: string
+	params?: object
+}
+
 const Stack = createStackNavigator();
 
 export default function Router() {
