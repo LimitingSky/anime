7cd85182dd5bbaf67361a090c6515801cac9f888
Luis Garcia <un.garcia9719@gmail.com>
2021-06-11 11:00:03 -0600

    add spaces const, dimensions and colors

---
+++ src/assets/colors/index.ts
@@ -0,0 +1,7 @@
+export const ORANGE = "rgba(220,109,26,1)";
+export const ORANGE_LIGHT = "rgba(252,245,240,1)"
+export const GREY = "rgb(227,227,227)"
+export const GREY_LIGHT = "rgb(246,244,248)"
+export const GREY_DARK = "rgb(146,147,157)"
+export const BLACK = "rgb(24,24,24)"
+export const WHITE = "rgb(255,255,255)"
---
+++ src/assets/const/index.ts
@@ -0,0 +1,14 @@
+interface ISpaces {
+	[x: string]: number
+}
+export const SPACES:ISpaces = {
+	xs: 4,
+	SM: 8,
+	MD: 16,
+	LG:	32,
+	XL: 64,
+	XXL: 128,
+}
+
+export const WIDTH_DESIGN:number = 1125
+export const HEIGHT_DESIGN:number = 2436
